0dfe6ec10457952cbae8a835c819b3239da2f06a
postmodernist1488 <elitic.pantheism@gmail.com>
2022-03-26 01:58:15 +0300

    You can now reset the game with 'R' and use question marks!

--- cell.py
+++ cell.py
@@ -1,36 +1,52 @@
+from typing import Deque
 import pyglet
+from pyglet.image import load
+from collections import deque
 
 class Cell(pyglet.sprite.Sprite):
-    unopenned_image = pyglet.image.load('images/unopenned.png')
-    bomb_image = pyglet.image.load('images/bomb.png')
-    zero_image = pyglet.image.load('images/zero.png')
-    one_image = pyglet.image.load('images/one.png')
-    two_image = pyglet.image.load('images/two.png')
-    three_image = pyglet.image.load('images/three.png')
-    four_image = pyglet.image.load('images/four.png')
-    five_image = pyglet.image.load('images/five.png')
-    six_image = pyglet.image.load('images/six.png')
-    seven_image = pyglet.image.load('images/seven.png')
-    eight_image = pyglet.image.load('images/eight.png')
-    flag_image = pyglet.image.load('images/flag.png')
-    values_dict = {0:zero_image, 1:one_image, 2:two_image, 3:three_image, 4:four_image, 5:five_image, 6:six_image, 7:seven_image, 8:eight_image, 'b':bomb_image}
+    unopenned_image = load('images/unopenned.png')
+    bomb_image = load('images/bomb.png')
+    zero_image = load('images/zero.png')
+    one_image = load('images/one.png')
+    two_image = load('images/two.png')
+    three_image = load('images/three.png')
+    four_image = load('images/four.png')
+    five_image = load('images/five.png')
+    six_image = load('images/six.png')
+    seven_image = load('images/seven.png')
+    eight_image = load('images/eight.png')
+    flag_image = load('images/flag.png')
+    question_mark_image = load('images/question_mark.png')
+    values_dict = {0:zero_image, 
+                    1:one_image, 
+                    2:two_image, 
+                    3:three_image, 
+                    4:four_image, 
+                    5:five_image, 
+                    6:six_image, 
+                    7:seven_image, 
+                    8:eight_image, 
+                    'b':bomb_image} 
+    rmb_states_dict = {'x':unopenned_image, 
+                        'f':flag_image, 
+                        '?':question_mark_image}
 
     def __init__(self, value=0, x=0, y=0, scale=1, batch=None, group=None):
         self.value = value
         self.openned = False 
-        self.flagged = False
+        self.rmb_state = 'x'
+        self.rmb_states_deque = deque(['x', 'f', '?'])
         super(Cell, self).__init__(Cell.unopenned_image, x=x, y=y, batch=batch, group=group)
         self.scale = scale
 
     def open(self):
-        print(f'cell {self.x, self.y} has been opened')
-        self.openned = True
-        self.image = Cell.values_dict[self.value]
+        if self.rmb_state == 'x':
+            self.openned = True
+            self.image = Cell.values_dict[self.value]
 
-    def flag(self):
+    def on_rmb(self):
         if not self.openned:
-            self.flagged = 1 - self.flagged
-            if self.flagged:
-                self.image = Cell.flag_image
-            else:
-                self.image = Cell.unopenned_image
+            self.rmb_states_deque.rotate(-1)
+            self.rmb_state = self.rmb_states_deque[0]
+            self.image = self.rmb_states_dict[self.rmb_states_deque[0]]
+            
--- main.py
+++ main.py
@@ -9,28 +9,36 @@ from cell import Cell
 #ресет и менюшки
 #в конце показывать все бомбы
 #запись рекордом по времени в сейв файл
+#взрывы при проигранной игре
 
 batch = pyglet.graphics.Batch()
 game_window = Window(450, 600, "Сапёр")
 
-
-class Timer(pyglet.text.Label):
+#таймер для игры
+class Timer(pyglet.text.Label):           
     def __init__(self, *args, **kwargs):
         super(Timer, self).__init__('00:00', *args, **kwargs)
-        self.start_time = time.time()
         
+        
+    def start_timer(self):
+        self.text = '00:00'
+        self.start_time = time.time()
+        pyglet.clock.schedule_interval(self.update, 0.5)
+
+    def stop_timer(self):
+        pyglet.clock.unschedule(self.update)
 
     def update(self, dt):
         cur_time =  time.time() - self.start_time
         self.text = '{mins:0>2}:{secs:0>2}'.format(mins = int(cur_time / 60), secs = int(cur_time % 60))
         
-
+#класс игры в сапер
 class Main_game:
     def __init__(self):
         self.game_height = 10
         self.game_width = 10
         self.cell_size = 45
-        self.game_active = True
+        self.game_state = 'Игра'
         self.game_started = False
         self.game_timer = Timer(x=game_window.width // 2 - 200, y=game_window.height - 130, font_size=32, batch=batch)
         self.game_state_label = pyglet.text.Label(text='Игра', font_name = 'Consolas', bold=True, anchor_x='center', align='center', x=game_window.width // 2, y=game_window.height - 50, batch=batch)
@@ -42,6 +50,7 @@ class Main_game:
 
         game_window.push_handlers(self)
         
+    #создание двумерного списка(матрицы), в соответствии с которой создается 'минное поле'
     def create_minesweeper_matrix(self):
         self.mines_count = 0
         minesweeper_matrix = [[0] * (self.game_width + 2) for _ in range(self.game_height + 2)]
@@ -59,10 +68,8 @@ class Main_game:
                     neighbouring_cells = minesweeper_matrix[i - 1][j - 1] + minesweeper_matrix[i - 1][j] + minesweeper_matrix[i - 1][j + 1] + minesweeper_matrix[i][j - 1] + \
                 minesweeper_matrix[i][j + 1] + minesweeper_matrix[i + 1][j - 1] + minesweeper_matrix[i + 1][j] + minesweeper_matrix[i + 1][j + 1]
                     self.minesweeper_matrix[i - 1][j - 1] = neighbouring_cells
-        for row in minesweeper_matrix:
-            print(*row)
-        print('-'*20)
 
+    #создание 'минного поля' из спрайтов 
     def create_minefield(self):
         self.cells = []
         for y in range(0, self.game_width * self.cell_size, self.cell_size):
@@ -73,16 +80,16 @@ class Main_game:
             self.cells.append(row)
 
     def on_mouse_press(self, x, y, button, modifiers):
-        if self.game_active:
+        if self.game_state == 'Игра':
             if button == pyglet.window.mouse.LEFT:
                 try:
-                    self.cells[y // self.cell_size][x // self.cell_size].open()
-                except Exception: 
+                    self.cells[y // self.cell_size][x // self.cell_size].open() #лкм - открытие клетки
+                except IndexError: 
                     pass
             elif button == pyglet.window.mouse.RIGHT:
                 try:
-                    self.cells[y // self.cell_size][x // self.cell_size].flag()
-                except Exception:
+                    self.cells[y // self.cell_size][x // self.cell_size].on_rmb() #правая кнопка - переключение между закрытой клеткой, флагом и знаком вопроса
+                except IndexError:
                     pass
     def on_key_press(self, symbol, modifiers):
         if symbol == key.C:  #читы
@@ -90,32 +97,42 @@ class Main_game:
                 for cell in row:
                     if cell.value != 'b':
                         cell.open()
-            
+        elif symbol == key.R:
+            self.game_reset()
     def update(self, dt):
         openned_counter = 0
-        if self.game_active:
+        if self.game_state == 'Игра':
             for row in self.cells:
                 for cell in row:
                     if cell.openned:
                         openned_counter += 1
                         if not self.game_started:
                             self.game_started = True
-                            pyglet.clock.schedule_interval(self.game_timer.update, 0.5)
+                            self.game_timer.start_timer()
                         if cell.value == 'b':
-                            self.game_active = False
                             self.game_state = 'Вы проиграли!'
+                            return
             if openned_counter + self.mines_count == self.game_height * self.game_width:
-                self.game_active = False
                 self.game_state = 'Вы выиграли!'
-        else:
+                
+        else:                                                                      #конец игры
             self.game_state_label.text = self.game_state
             pyglet.clock.unschedule(self.update)
-            pyglet.clock.unschedule(self.game_timer.update)
+            self.game_timer.stop_timer()
 
-        
+    def game_reset(self):
+        self.game_state = 'Игра'
+        self.game_started = False
+        self.game_state_label.text = self.game_state
+        self.create_minesweeper_matrix()
+        print('-' * 20)
+        for row in self.minesweeper_matrix:
+            print(*row)
+        self.create_minefield()
+        self.game_timer.start_timer()
+        pyglet.clock.schedule_interval(self.update, 1/60)
 
 main_game = Main_game()
-
 
 @game_window.event
 def on_draw():
