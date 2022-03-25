aefd09556b48511b8d3c522de4f2f3695cacaa7f
postmodernist1488 <elitic.pantheism@gmail.com>
2022-03-26 02:25:00 +0300

    comments and slight optimizations

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ import time
 from cell import Cell
 
 #TODO: открывание всех пустых клеток, соседних с открытой
-#флажки и знаки вопроса, штука на среднюю кнопку мыши
-#ресет и менюшки
+#штука на среднюю кнопку мыши
+#менюшки
 #в конце показывать все бомбы
 #запись рекордом по времени в сейв файл
 #взрывы при проигранной игре
 
+#-------------- Константы ------------------------------------------------
+GAME = 'Игра'
+WIN = 'Вы выиграли!'
+LOSS = 'Вы проиграли!'
+
+
+#---------------- Игра ---------------------------------------------------
 batch = pyglet.graphics.Batch()
 game_window = Window(450, 600, "Сапёр")
 
@@ -38,10 +45,10 @@ class Main_game:
         self.game_height = 10
         self.game_width = 10
         self.cell_size = 45
-        self.game_state = 'Игра'
+        self.game_state = GAME
         self.game_started = False
         self.game_timer = Timer(x=game_window.width // 2 - 200, y=game_window.height - 130, font_size=32, batch=batch)
-        self.game_state_label = pyglet.text.Label(text='Игра', font_name = 'Consolas', bold=True, anchor_x='center', align='center', x=game_window.width // 2, y=game_window.height - 50, batch=batch)
+        self.game_state_label = pyglet.text.Label(text=GAME, font_name = 'Consolas', bold=True, anchor_x='center', align='center', x=game_window.width // 2, y=game_window.height - 50, batch=batch)
         self.create_minesweeper_matrix()
         for row in self.minesweeper_matrix:
             print(*row)
@@ -80,48 +87,54 @@ class Main_game:
             self.cells.append(row)
 
     def on_mouse_press(self, x, y, button, modifiers):
-        if self.game_state == 'Игра':
+        if self.game_state == GAME:
             if button == pyglet.window.mouse.LEFT:
                 try:
-                    self.cells[y // self.cell_size][x // self.cell_size].open() #лкм - открытие клетки
+                    self.cells[y // self.cell_size][x // self.cell_size].open()  #лкм - открытие клетки
+                    if not self.game_started:
+                        self.game_start()                
                 except IndexError: 
                     pass
             elif button == pyglet.window.mouse.RIGHT:
                 try:
                     self.cells[y // self.cell_size][x // self.cell_size].on_rmb() #правая кнопка - переключение между закрытой клеткой, флагом и знаком вопроса
+                    if not self.game_started:
+                        self.game_start()
                 except IndexError:
                     pass
     def on_key_press(self, symbol, modifiers):
-        if symbol == key.C:  #читы
+        if symbol == key.C:  #читы - победа по нажатию кнопки
             for row in self.cells:
                 for cell in row:
                     if cell.value != 'b':
                         cell.open()
-        elif symbol == key.R:
+        elif symbol == key.R: #ресет игры
             self.game_reset()
-    def update(self, dt):
+
+    def update(self, dt): #метод обновления состояния игры
         openned_counter = 0
-        if self.game_state == 'Игра':
+        if self.game_state == GAME:
             for row in self.cells:
                 for cell in row:
                     if cell.openned:
                         openned_counter += 1
-                        if not self.game_started:
-                            self.game_started = True
-                            self.game_timer.start_timer()
                         if cell.value == 'b':
-                            self.game_state = 'Вы проиграли!'
+                            self.game_state = LOSS
                             return
             if openned_counter + self.mines_count == self.game_height * self.game_width:
-                self.game_state = 'Вы выиграли!'
+                self.game_state = WIN
                 
         else:                                                                      #конец игры
             self.game_state_label.text = self.game_state
             pyglet.clock.unschedule(self.update)
             self.game_timer.stop_timer()
 
-    def game_reset(self):
-        self.game_state = 'Игра'
+    def game_start(self):
+        self.game_started = True
+        self.game_timer.start_timer()
+
+    def game_reset(self): #название говорит само за себя
+        self.game_state = GAME
         self.game_started = False
         self.game_state_label.text = self.game_state
         self.create_minesweeper_matrix()
@@ -138,7 +151,6 @@ main_game = Main_game()
 def on_draw():
     game_window.clear()
     batch.draw()
-
 
 
 
